96460a8b28d5ebea2b0d3af0dec6ec15fc749769
Linden Chiu <hchiu87@gmail.com>
2020-01-08 13:28:52 -0800

    image button refactor

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 const Square = React.memo(props => {
    let param = props.piece;
    let mark = props.mark;
+   let pieceImage ="";
 
    if (mark === undefined || mark === null) {
      mark = "board";
    }
 
-   if (param === undefined || param === null ) {
-     return (
+   if (param) {
+     pieceImage = `./assets/pieces/${props.piece.name}.png`;
+   } 
+
+  return (
      
      <div onClick={props.handleClick} >
       <button
@@ -17,29 +21,15 @@ const Square = React.memo(props => {
         className={"square img" + props.id} 
         x={props.row} 
         y={props.col}>
+        <img 
+          src={pieceImage} 
+          className={"piece"}
+        >
+        </img> 
       </button>
     </div>
 
      )
-   } else {
-     return (
-
-      <div onClick={props.handleClick} >
-        <button className={"square img" + props.id} >
-          <img
-            id={mark}
-            src={`./assets/pieces/${props.piece.name}.png`}
-            className="piece" 
-            name={props.piece.name} 
-            x={props.row} 
-            y={props.col}
-          >
-          </img>
-      </button>
-    </div>
-  );
-
-   }
 });
 
 
